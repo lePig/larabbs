0c3258aef5cf611252fc604c56994afaa83b592a
caodapeng <caodapeng@kuaidaoapp.com>
2018-03-12 18:31:06 +0800

    渲染话题详情页内容及样式调整

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -51,7 +51,7 @@ class TopicsController extends Controller
         $topic->user_id = Auth::id();
 		// $topic = Topic::create($request->all());
         $topic->save();
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('message', '发表成功');
 	}
 
 	public function edit(Topic $topic)
@@ -65,7 +65,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('message', '更新成功');
 	}
 
 	public function destroy(Topic $topic)
@@ -73,7 +73,7 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('topics.index')->with('message', '已成功删除了话题');
 	}
 
     /**
