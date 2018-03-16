2fc74408b27fb80617702395fa09f0070e87194e
caodapeng <caodapeng@kuaidaoapp.com>
2018-03-16 17:47:33 +0800

    用户角色及权限管理

--- resources/views/topics/_topic_list.blade.php
+++ resources/views/topics/_topic_list.blade.php
@@ -1,11 +1,10 @@
 @if (count($topics))
-
     <ul class="media-list">
         @foreach ($topics as $topic)
             <li class="media">
                 <div class="media-left">
-                    <a href="{{ route('users.show', [$topic->user_id]) }}">
-                        <img class="media-object img-thumbnail" style="width: 52px; height: 52px;" src="{{ $topic->user->avatar }}" title="{{ $topic->user->name }}">
+                    <a href="{{ route('users.show', [$topic->user_id ?:'']) }}">
+                        <img class="media-object img-thumbnail" style="width: 52px; height: 52px;" src="{{ $topic->user->avatar or '' }}" title="{{ $topic->user->name or '' }}">
                     </a>
                 </div>
 
@@ -28,9 +27,9 @@
                         </a>
 
                         <span> • </span>
-                        <a href="{{ route('users.show', [$topic->user_id]) }}" title="{{ $topic->user->name }}">
+                        <a href="{{ route('users.show', [$topic->user_id]) }}" title="{{ $topic->user->name or '' }}">
                             <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-                            {{ $topic->user->name }}
+                            {{ $topic->user->name or '' }}
                         </a>
                         <span> • </span>
                         <span class="glyphicon glyphicon-time" aria-hidden="true"></span>
