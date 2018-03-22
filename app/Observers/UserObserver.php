07ea720d18784746a7f93487f6113d6f4620f525
caodapeng <caodapeng@kuaidaoapp.com>
2018-03-22 10:24:12 +0800

    使用用户模型监控器给没有头像用户赋予默认头像

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -18,4 +18,17 @@ class UserObserver
     {
         //
     }
+
+    /**
+     * 模型监控器 对没有头像的用户赋予默认头像
+     * (avatar: https://fsdhubcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png)
+     * @date   2018-03-22
+     * @return [type]     [description]
+     */
+    public function saving(User $user)
+    {
+        if (empty($user->avatar)) {
+            $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        }
+    }
 }
